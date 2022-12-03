f4e0417dc24c1abd44f0979029811728991abd17
shubham-tam <tamrakarshubham23@gmail.com>
2022-12-04 00:50:15 +0545

    Login with  local storage added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import LoginWithLocalStorage from "./components/Auth/LoginWithLocalStorage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<LoginWithLocalStorage />);
