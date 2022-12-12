9923a507a7cf06cd220445da723acdb0b940874a
shubham-tam <tamrakarshubham23@gmail.com>
2022-12-12 10:43:20 +0545

    Added copyright on footer

--- src/components/Pages/Footer.js
+++ src/components/Pages/Footer.js
@@ -4,7 +4,7 @@ import classes from "..//..//assets/style/footer.module.css";
 export const Footer = () => {
   return (
     <div className={classes.footer}>
-      <h4>Copyright © 2022</h4>
+      <h4>Copyright &copy; 2022</h4>
     </div>
   );
 };
